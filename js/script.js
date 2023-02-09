2dbe413222d23d7ef4575749c034d9988a623c78
OlenaHras <alena.hras@gmail.com>
2023-02-09 11:19:44 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -104,15 +104,19 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const modal = document.querySelector(".modal");
   const openModal = document.querySelectorAll("[data-modal]");
-  const closeModal = document.querySelector("[data-close]");
+  // const closeModal = document.querySelector("[data-close]");
   openModal.forEach((btn) => btn.addEventListener("click", openModalWindow));
 
   function openModalWindow() {
-    modal.style.display = "block";
-    closeModal.addEventListener("click", closeModalWindow);
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    // clearInterval(modalTimerId);
+    // modal.style.display = "block";
+    // closeModal.addEventListener("click", closeModalWindow);
 
     modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
+      if (e.target === modal || e.target.getAttribute("data-close") == "") {
         closeModalWindow();
       }
     });
@@ -123,15 +127,16 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     });
     document.body.style.overflow = "hidden";
-    clearInterval(modalTimetId);
+    clearInterval(modalTimerId);
   }
 
   function closeModalWindow() {
-    modal.style.display = "none";
+    modal.classList.add("hide");
+    modal.classList.remove("show");
     document.body.style.overflow = "";
   }
 
-  const modalTimetId = setTimeout(openModalWindow, 10000);
+  const modalTimerId = setTimeout(openModalWindow, 50000);
 
   function showModalByScroll() {
     if (
@@ -143,4 +148,348 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   window.addEventListener("scroll", showModalByScroll);
+
+  //Классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+      element.innerHTML = ` 
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+          `;
+
+      this.parent.append(element);
+    }
+  }
+
+  const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  // getResource("http://localhost:3000/menu").then((data) => {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuCard(
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price,
+  //       ".menu .container"
+  //     ).render();
+  //   });
+  // });
+
+  axios.get("http://localhost:3000/menu").then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
+
+  //Forms
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "img/spinner.svg",
+    success: "Спасибо! Скоро мы с Вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
+
+  forms.forEach((item) => {
+    bindPostData(item);
+  });
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data,
+    });
+
+    return res.json();
+  };
+
+  function bindPostData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("img");
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `;
+      // statusMessage.textContent = message.loading;
+      // form.append(statusMessage);
+      form.insertAdjacentElement("afterend", statusMessage);
+
+      // const request = new XMLHttpRequest();
+      // request.open("POST", "server.php");
+
+      // request.setRequestHeader("Content-type", "application/json");
+      const formData = new FormData(form);
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      // const object = {};
+      // formData.forEach(function (value, key) {
+      //   object[key] = value;
+      // });
+
+      // const json = JSON.stringify(object);
+
+      // request.send(json);
+
+      postData("http://localhost:3000/requests", json)
+        .then((data) => {
+          console.log(data);
+          showThanksModal(message.success);
+          form.reset();
+          statusMessage.remove();
+        })
+        .catch(() => {
+          showThanksModal(message.failure);
+        })
+        .finally(() => {
+          form.reset();
+        });
+
+      // request.addEventListener("load", () => {
+      //   if (request.status === 200) {
+      // console.log(request.response);
+      // showThanksModal(message.success);
+      // form.reset();
+
+      // statusMessage.remove();
+      //   } else {
+      //     showThanksModal(message.failure);
+      //   }
+      // });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
+
+    prevModalDialog.classList.add("hide");
+    openModalWindow();
+
+    const thanksModal = document.createElement("div");
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
+    <div class="modal__content">
+      <div class="modal__close" data-close>&times;</div>
+      <div class="modal__title">${message}</div>
+    </div>
+
+    `;
+
+    document.querySelector(".modal").append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.add("hide");
+      closeModalWindow();
+    }, 4000);
+  }
+
+  fetch("http://localhost:3000/menu").then((data) => data.json());
+  // .then((res) => console.log(res));
+
+  //Slider
+  const slides = document.querySelectorAll(".offer__slide");
+  const slider = document.querySelector(".offer__slider");
+  const slidesWrapper = document.querySelector(".offer__slider-wrapper");
+  const slidesField = document.querySelector(".offer__slider-inner");
+  const width = window.getComputedStyle(slidesWrapper).width;
+  const prev = document.querySelector(".offer__slider-prev");
+
+  const next = document.querySelector(".offer__slider-next");
+  const currentSlide = document.querySelector("#current");
+  const total = document.querySelector("#total");
+  let slideIndex = 1;
+  let offset = 0;
+
+  if (slides.length < 10) {
+    total.textContent = "0" + slides.length;
+    currentSlide.textContent = "0" + slideIndex;
+  } else {
+    total.textContent = slides.length;
+    currentSlide.textContent = slideIndex;
+  }
+
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  slider.style.position = "relative";
+
+  const indicators = document.createElement("ol");
+  const dots = [];
+  indicators.classList.add("carousel-indicators");
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i += 1) {
+    const dot = document.createElement("li");
+    dot.classList.add("dot");
+    dot.setAttribute("data-slide-to", i + 1);
+    if (i === 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, "");
+  }
+
+  next.addEventListener("click", () => {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += deleteNotDigits(width);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    changeCounter(slideIndex);
+    sliderDots(slideIndex);
+    // dots.forEach((dot) => (dot.style.opacity = ".5"));
+    // dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  prev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = deleteNotDigits(width) * (slides.length - 1);
+    } else {
+      offset -= deleteNotDigits(width);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    changeCounter(slideIndex);
+    sliderDots(slideIndex);
+  });
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+
+      slideIndex = slideTo;
+      offset = deleteNotDigits(width) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      changeCounter(slideIndex);
+      sliderDots(slideIndex);
+    });
+  });
+
+  function changeCounter(index) {
+    if (index < 10) {
+      currentSlide.textContent = "0" + index;
+    } else {
+      currentSlide.textContent = index;
+    }
+  }
+
+  function sliderDots(index) {
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[index - 1].style.opacity = 1;
+  }
+
+  // function showSlides(n) {
+  //   if (n > slides.length) {
+  //     slideIndex = 1;
+  //   }
+  //   if (n < 1) {
+  //     slideIndex = slides.length;
+  //   }
+
+  //   slides.forEach((slide) => (slide.style.display = "none"));
+  //   slides[slideIndex - 1].style.display = "block";
+  // }
+  // showSlides(slideIndex);
+  // // currentSlide.textContent = 01;
+
+  // function onPrevClick() {
+  //   slideIndex -= 1;
+  //   showSlides(slideIndex);
+  //   changeCounter(slideIndex);
+  // }
+
+  // function onNextClick() {
+  //   slideIndex += 1;
+  //   showSlides(slideIndex);
+  //   changeCounter(slideIndex);
+  // }
+
+  // function changeCounter(index) {
+  //   if (index < 10) {
+  //     currentSlide.textContent = "0" + index;
+  //   } else {
+  //     currentSlide.textContent = index;
+  //   }
+  // }
+  // changeCounter(slideIndex);
 });
